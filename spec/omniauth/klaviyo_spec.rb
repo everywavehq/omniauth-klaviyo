0b31a8b99a84f60292bf5348f68b5431f952e4cd
Morad Boukhari <moradboukhari@gmail.com>
2024-11-22 15:43:07 +0200

    Update klaviyo_spec.rb

--- spec/omniauth/klaviyo_spec.rb
+++ spec/omniauth/klaviyo_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe Omniauth::Klaviyo do
   it "has a version number" do
     expect(Omniauth::Klaviyo::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
